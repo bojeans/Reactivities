7b276530ecdfc0c932612ceee9d912d856651870
Chris Anderson <chris.d.anderson87@gmail.com>
2024-02-16 12:13:05 +1300

    fixed db connection & cors issue. working state

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+interface Activity {
+  id: string;
+  title: string;
+  date: string;
+  description: string;
+  category: string;
+  city: string;
+  venue: string;
+}
 
 const App = () => {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
+      setActivities(response.data);
+    });
+  }, []);
+
   return (
-    <>
-      <h1>title</h1>
-    </>
+    <div>
+      <h1>Reactivities</h1>
+      <ul>
+        {activities.map((activity: Activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
